01a736d5da26447b7a39ffe0b0b998e5154de2d1
Tristan Wong <tristanwong321@hotmail.com>
2022-11-23 16:58:51 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+import {useState} from 'react';
+
 function App() {
+
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: ""
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleFirstNameChange = e => { setValues({...values, firstName: e.target.value})};
+  const handleLastNameChange = e => { setValues({...values, lastName: e.target.value})};
+  const handleEmailChange = e => { setValues({...values, email: e.target.value})};
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // if (values.firstName !== "" && values.lastName !== "" && values.email !== ""){
+    if (values.firstName && values.lastName && values.email){
+      setValid(true);
+    }
+    setSubmitted(true);
+  }
+
   return (
-    <div className="App">
-      
+    <div className="form-container">
+      <form className='registration-form' onSubmit={handleSubmit}>
+        {submitted && valid ? <div className='success-msg'>Success, thanks for registering!</div>: null }
+        <input 
+          className='form-field' 
+          type="text" 
+          placeholder='First Name' 
+          name='firstName' 
+          value={values.firstName}
+          onChange={handleFirstNameChange}
+        />
+        { submitted && !values.firstName ? <span>Please enter a first name</span> : null }
+
+        <input 
+          className='form-field' 
+          type="text" 
+          placeholder='Last Name' 
+          name='lastName' 
+          value={values.lastName}
+          onChange={handleLastNameChange}
+        />
+        { submitted && !values.lastName ? <span>Please enter a last name</span> : null}
+
+        <input 
+          className='form-field' 
+          type="text" 
+          placeholder='Email' 
+          name='email' 
+          value={values.email}
+          onChange={handleEmailChange}
+        />
+        { submitted && !values.email ? <span>Please enter an email</span> : null }
+        <button>Register</button>
+      </form>
     </div>
   );
 }
